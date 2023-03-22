8764800bcc2646dc2aeae9f12cc707fc593cedb8
Chey04 <vchukwukere123@gmail.com>
2023-03-22 07:49:54 -0700

    compiled code and created exe

--- monty.h
+++ monty.h
@@ -1,27 +1,17 @@
-#ifndef MONTY_H
-#define MONTY_H
+#ifndef MONTY
+#define MONTY
+#define _POSIX_C_SOURCE  200809L
+#define _GNU_SOURCE
+
 
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <ctype.h>
-#include <string.h>
-#include <fcntl.h>
 #include <sys/types.h>
-
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
 
 /**
  * struct stack_s - doubly linked list representation of a stack (or queue)
@@ -30,13 +20,13 @@ typedef struct instruction_s
  * @next: points to the next element of the stack (or queue)
  *
  * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO
+ * for stack, queues, LIFO, FIFO Holberton project
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -61,7 +51,21 @@ typedef struct globals
 	char *buffer;
 } global_t;
 
-extern global_t vglo
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO Holberton project
+ */
+typedef struct instruction_s
+{
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+extern global_t vglo;
 
 /* opcode_instructuions*/
 void _push(stack_t **stack, unsigned int line_number);
